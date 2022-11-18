778cbc4b3ce226ea0054ea9bce12bdaf98d40f3c
IvanGalenko <98175502+IvanGalenko@users.noreply.github.com>
2022-11-18 15:44:31 +0300

    Добавлены первые наброски проекта

--- Program.cs
+++ Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //(string Name, string LastName, string[] favcolors) User = EnterUser();
+            //Console.WriteLine("Ваше имя: {0}",User.Name);
+            //Console.WriteLine("Ваша фамилия: {0}",User.LastName);
+            //Console.WriteLine("Длина массива: {0}", User.favcolors.GetUpperBound(0));
         }
+        static (string Name,
+            string LastName,
+            int Age,
+            bool HasPets,
+            string[] Pets,
+            string[] favcolors)
+            EnterUser()
+        {
+            (string Name, string LastName, int age, bool HasPets, string[] Pets, string[] favcolors) User;
+            Console.Write("Введите Ваше имя: ");
+            string Name = Console.ReadLine();
+
+        }
+
+
+
+        //(string name, string lname, string[] favcolors) User;
+        //User.name = "Иван";
+        //    User.lname = "Галенко";
+        //    User.favcolors = new string[3];
+        //    User.favcolors[0] = "Red";
+        //    User.favcolors[1] = "Black";
+        //    User.favcolors[2] = "Blue";
+        //    return User;
+
+
     }
 }
